2a2a5da4441cbf674df79a10604f50f0e839c27c
Welington Braga <welrbraga@yahoo.com>
2014-06-02 22:04:13 -0300

    Adição da versão 7u60 do JRE

--- wrb-getjava.sh
+++ wrb-getjava.sh
@@ -94,6 +94,11 @@ URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=83376"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=83374"
 VERSION="7u51"
 
+#2014-06-02 JRE SE 7u60 - O arquivo para 32bits não é executável
+URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=90214"
+URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=90216"
+VERSION="7u51"
+
 ARCH=`uname -m`
 if [ "${ARCH}" == "x86_64" ]; then
     echo -n "Selecionada versão 64bits"
