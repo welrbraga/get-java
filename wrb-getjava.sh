2ea89628c3d1a7b1d7d8719c1814621dfa2e8029
Welington R Braga <welrbraga@yahoo.com>
2014-10-19 21:34:50 -0200

    Melhoraria na estrutura do script com implementação de novs funções.
    As variaveis de definição de versão da JRE foram movidas para funções individuais
    
    A condicional para verificar a arquitetura do sistema (32bits/64bits) foi movida
    para a nova função get_arch

--- wrb-getjava.sh
+++ wrb-getjava.sh
@@ -8,113 +8,157 @@
 
 #URLS obtidas em http://www.java.com/pt_BR/download/manual.jsp
 
+function jre6u30() {
 #2011-12-30 JRE SE 6u30
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=58119"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=58117"
 VERSION="6u30"
+}
 
+function jre6u31() {
 #2012-03-02 JRE SE 6u31
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=59623"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=59621"
 VERSION="6u31"
+}
 
+function jre6u34() {
 #2012-08-24 JRE SE 6u34
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=66975"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=66973"
 VERSION="6u34"
+}
 
+function jre6u35() {
 #2012-10-14 JRE SE 6u35
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=68286"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=68284"
 VERSION="6u35"
+}
 
+function jre6u38() {
 #2012-12-27 JRE SE 6u38
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=71305"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=71303"
 VERSION="6u38"
+}
 
+function jre7u4() {
 #2012-03-02 JRE SE 7u4 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=63204"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=63202"
 VERSION="7u4"
+}
 
+function jre7u6() {
 #2012-08-21 JRE SE 7u6 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=67388"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=67386"
 VERSION="7u6"
+}
 
+function jre7u7() {
 #2012-10-14 JRE SE 7u7 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=68236"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=66234"
 VERSION="7u7"
+}
 
+function jre7u10() {
 #2012-12-27 JRE SE 7u10 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=71828"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=71826"
 VERSION="7u10"
+}
 
+function jre7u11() {
 #2013-01-19 JRE SE 7u11 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=73134"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=73132"
 VERSION="7u11"
+}
 
+function jre7u15() {
 #2013-02-24 JRE SE 7u15 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=74774"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=74772"
 VERSION="7u15"
+}
 
+function jre7u17() {
 #2013-03-19 JRE SE 7u17 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=75252"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=75250"
 VERSION="7u17"
+}
 
+function jre7u21() {
 #2013-04-19 JRE SE 7u21 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=76853"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=76851"
 VERSION="7u21"
+}
 
+function jre7u25() {
 #2013-06-26 JRE SE 7u25 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=78697"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=78695"
 VERSION="7u25"
+}
 
+function jre7u40() {
 #2013-09-12 JRE SE 7u40 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=80805"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=80803"
 VERSION="7u40"
+}
 
+function jre7u45() {
 #2013-10-20 JRE SE 7u45 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=81812"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=81810"
 VERSION="7u45"
+}
 
-
+function jre7u51() {
 #2014-01-23 JRE SE 7u51 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=83376"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=83374"
 VERSION="7u51"
+}
 
+function jre7u60() {
 #2014-06-02 JRE SE 7u60 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=90216"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=90214"
 VERSION="7u60"
+}
 
+function jre7u65() {
 #2014-08-01 JRE SE 7u65 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=92496"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=92494"
 VERSION="7u65"
+}
 
+function jre7u67() {
 #2014-08-21 JRE SE 7u67 - O arquivo para 32bits não é executável
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=95116"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=95114"
 VERSION="7u67"
+}
 
+function jre7u71() {
 #2014-10-14 JRE SE 7u71 - O arquivo para 32bits não é executável 
 # Aparentemente ultimo release do JAVA 7
 URL64="http://javadl.sun.com/webapps/download/AutoDL?BundleId=97800"
 URL32="http://javadl.sun.com/webapps/download/AutoDL?BundleId=97798"
 VERSION="7u71"
+}
 
+##############################
+
+function set_arch() {
 ARCH=`uname -m`
 if [ "${ARCH}" == "x86_64" ]; then
     echo -n "Selecionada versão 64bits"
@@ -122,33 +166,23 @@ if [ "${ARCH}" == "x86_64" ]; then
     ARCHPLUGIN="amd64"
 # desde  a versao 12.04 o ubuntu usa o arquivo libnpjp2.so como plugin
     FILEPLUGIN="lib/${ARCHPLUGIN}/libnpjp2.so"
-    #Debian e Ubuntu Lucid usem este:
-#    FILEPLUGIN="lib/${ARCHPLUGIN}/libjavaplugin_jni.so"
-#    if [ -f /etc/lsb-release ]; then
-#        . /etc/lsb-release
-#        if [ "$DISTRIB_ID" == "Ubuntu" ] && [ "$DISTRIB_RELEASE" == "12.04" ]; then
-#            echo -n " para Ubuntu 12.04"
-#            FILEPLUGIN="lib/${ARCHPLUGIN}/libnpjp2.so"
-#        fi
-#        if [ "$DISTRIB_ID" == "Ubuntu" ] && [ "$DISTRIB_RELEASE" == "13.04" ]; then
-#            echo -n " para Ubuntu 13.04"
-#            FILEPLUGIN="lib/${ARCHPLUGIN}/libnpjp2.so"
-#        fi
-        echo ""
-#
-#    fi
+    echo ""
 else
     echo "Selecionada versão 32bits"
     URL=${URL32}
     ARCHPLUGIN="i386"
     FILEPLUGIN="lib/${ARCHPLUGIN}/libnpjp2.so"
 fi
+}
 
 if [ "$UID" != "0" ]; then
 	echo "Você precisa ser administrador para conseguir instalar a máquina Java em seu sistema"
 	echo "Tente novamente com: sudo $0"
 	exit
 fi
+
+jre7u71
+set_arch
 
 echo "Configurando o diretório onde a nova versão do Java será armazenada"
 PATHJAVA="/opt/java-${ARCHPLUGIN}"
